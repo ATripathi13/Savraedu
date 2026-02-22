8db3d0ff8290e928ba9310fa746f4564338b52c3
Akshat Tripathi <141961701+ATripathi13@users.noreply.github.com>
2026-02-22 15:10:55 +0530

    deployment

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "sqlite:///./activities.db"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///./savradb.db"
+)
+
 
 engine = create_engine(
     DATABASE_URL, connect_args={"check_same_thread": False}
